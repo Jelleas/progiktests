aa96341142600196f4b55be898a105b0ea612f1e
jelleas <jelle.van.assema@gmail.com>
2017-09-11 22:23:42 +0200

    caesar tests

--- tests/python/caesarTest.py
+++ tests/python/caesarTest.py
@@ -3,27 +3,79 @@ import checkpy.lib as lib
 import checkpy.assertlib as asserts
 
 @t.test(0)
-def key0(test):
+def key1(test):
     def testMethod():
         import sys
-        sys.argv = ["caesar.py", "0"]
-        inputArgs = ["def"]
+        sys.argv = ["caesar.py", "1"]
+        inputArgs = ["a"]
         output = lib.outputOf(_fileName, inputArgs)
         line = lib.getLine(output, 0)
-        return asserts.contains(line, "def")
+        return asserts.contains(line, "b")
 
     test.test = testMethod
-    test.description = lambda : "encrypt def naar def met key = 0"
+    test.description = lambda : "encrypts a as b using 1 as key"
 
 @t.test(10)
+def key23(test):
+    def testMethod():
+        import sys
+        sys.argv = ["caesar.py", "23"]
+        inputArgs = ["barfoo"]
+        output = lib.outputOf(_fileName, inputArgs)
+        line = lib.getLine(output, 0)
+        return asserts.contains(line, "yxocll")
+
+    test.test = testMethod
+    test.description = lambda : "encrypts barfoo as yxocll using 23 as key"
+
+@t.test(20)
 def key3(test):
     def testMethod():
         import sys
         sys.argv = ["caesar.py", "3"]
-        inputArgs = ["abc"]
+        inputArgs = ["BARFOO"]
         output = lib.outputOf(_fileName, inputArgs)
         line = lib.getLine(output, 0)
-        return asserts.contains(line, "def")
+        return asserts.contains(line, "EDUIRR")
 
     test.test = testMethod
-    test.description = lambda : "encrypt abc naar def met key = 3"
+    test.description = lambda : "encrypts BARFOO as EDUIRR using 3 as key"
+
+@t.test(30)
+def key4(test):
+    def testMethod():
+        import sys
+        sys.argv = ["caesar.py", "4"]
+        inputArgs = ["BaRFoo"]
+        output = lib.outputOf(_fileName, inputArgs)
+        line = lib.getLine(output, 0)
+        return asserts.contains(line, "FeVJss")
+
+    test.test = testMethod
+    test.description = lambda : "encrypts BaRFoo as FeVJss using 4 as key"
+
+@t.test(40)
+def key65(test):
+    def testMethod():
+        import sys
+        sys.argv = ["caesar.py", "65"]
+        inputArgs = ["barfoo"]
+        output = lib.outputOf(_fileName, inputArgs)
+        line = lib.getLine(output, 0)
+        return asserts.contains(line, "onesbb")
+
+    test.test = testMethod
+    test.description = lambda : "encrypts barfoo as onesbb using 65 as key"
+
+@t.test(50)
+def invalidInput(test):
+    def testMethod():
+        import sys
+        sys.argv = ["caesar.py"]
+        inputArgs = ["foo"]
+        output = lib.outputOf(_fileName, inputArgs)
+        line = lib.getLine(output, 0)
+        return asserts.contains(line, "usage: python caesar.py key")
+
+    test.test = testMethod
+    test.description = lambda : "handles lack of argv[1]"
